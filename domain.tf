2dcd350ff386b2a8efe7a98d69487499964fbcbd
Scott Sickles <scottsickles@gmail.com>
2024-02-13 00:04:26 -0500

    add access policy

--- domain.tf
+++ domain.tf
@@ -4,7 +4,7 @@ data "ns_connection" "domain" {
 }
 
 locals {
-  domain_id         = data.ns_connection.domain.outputs.domain_id
+  domain_arn        = data.ns_connection.domain.outputs.domain_arn
   document_endpoint = data.ns_connection.domain.outputs.document_endpoint
   search_endpoint   = data.ns_connection.domain.outputs.search_endpoint
 }
