23fc598b107c2e8bef3ca3ac7b98228e6cb45da2
Scott Sickles <scottsickles@gmail.com>
2024-05-31 14:07:55 -0400

    add permissions to define an index field

--- access.tf
+++ access.tf
@@ -31,6 +31,7 @@ data "aws_iam_policy_document" "access" {
     actions = [
       "cloudsearch:DescribeDomains",
       "cloudsearch:DescribeIndexFields",
+      "cloudsearch:DefineIndexField",
     ]
   }
 }
